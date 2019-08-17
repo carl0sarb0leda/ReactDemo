936da3eea9db6d923054d163dcc637148d122698
carl0sarb0leda <carlosaepn@gmail.com>
2019-08-17 15:07:47 +1200

    Defining parameters for a simple react demo

---
+++ precache-manifest.75da3c860d2c343820f611680bfa9c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49ff093548b185006e1",
+    "url": "/demo-app/static/css/main.9aa44d15.chunk.css"
+  },
+  {
+    "revision": "c49ff093548b185006e1",
+    "url": "/demo-app/static/js/main.c49ff093.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e7f1b8ad339b1d7c190203b41da0a038",
+    "url": "/demo-app/index.html"
+  }
+];
